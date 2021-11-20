8b8e6b11fe345dfce3e80be4d222687cda8f7c0b
Jason Tang <jasontang14@hotmail.com>
2021-11-20 14:56:54 +0800

    eraser debug

--- js/eraser.js
+++ js/eraser.js
@@ -1,25 +1,26 @@
 class Erasing extends PaintFunction {
-    constructor(contextReal) {
-        super();
-        this.context = contextReal;
-    }
-    onMouseDown(coord) {
-        this.context.lineJoin = "round";
-        this.context.lineWidth = localStorage.size;
-        this.context.beginPath();
-        this.context.moveTo(coord[0], coord[1]);
-    }
-    onDragging(coord) {
-        this.draw(coord[0], coord[1]);
-        this.context.globalCompositeOperation = "destination-out";
-    }
-    onMouseUp() {
-        let lastMove = canvasReal.toDataURL();
-        undoDataStack.push(lastMove);
-        redoDataStack = [];
-    }
-    draw(x, y) {
-        this.context.lineTo(x, y);
-        this.context.stroke();
-    }
+  constructor(contextReal) {
+    super();
+    this.context = contextReal;
+  }
+  onMouseDown(coord) {
+    this.context.lineJoin = "round";
+    this.context.lineWidth = localStorage.size;
+    this.context.beginPath();
+    this.context.moveTo(coord[0], coord[1]);
+  }
+  onDragging(coord) {
+    this.draw(coord[0], coord[1]);
+    this.context.globalCompositeOperation = "destination-out";
+  }
+  onMouseUp() {
+    let lastMove = canvasReal.toDataURL();
+    undoDataStack.push(lastMove);
+    redoDataStack = [];
+    this.context.globalCompositeOperation = "source-over";
+  }
+  draw(x, y) {
+    this.context.lineTo(x, y);
+    this.context.stroke();
+  }
 }
